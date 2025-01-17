4b7ee4c14fc844cd22d74e1f9eb8b516f62b8ac9
mahanandhan <mahanandhanmanish@gmail.com>
2025-01-17 09:05:07 +0530

    added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const { resolve } = require('path');
+
 
 const app = express();
 const port = 3010;
 
+
+
 app.use(express.static('static'));
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
